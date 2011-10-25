aa9eba033ff00eca74816c30d9256ad484259f3e
Eric Nordmeyer <ejpres@ericnordmeyer.de>
2011-10-25 12:03:40 +0200

    adding gradients to headline

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,7 @@ class Post
 
   validates :title, :presence => true
   validates :body, :presence => true
+  belongs_to :user
   has_mongoid_attached_file :image, :styles => { :slide => "720x400#", :medium => "240x240#", :team => "240x320#", :thumb => "100x100#" }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User
 
   has_secure_password
   validates_presence_of :password, :on => :create
+  has_many :posts;
 end
