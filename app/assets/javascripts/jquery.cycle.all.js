f90364b5a988b7ab8194cce99b6870c0bc9c25cc
Eric Nordmeyer <ejpres@ericnordmeyer.de>
2011-12-09 13:19:41 +0100

    fixes for ie

--- app/assets/javascripts/jquery.cycle.all.js
+++ app/assets/javascripts/jquery.cycle.all.js
@@ -195,7 +195,7 @@ function handleArguments(cont, options, arg2) {
 };
 
 function removeFilter(el, opts) {
-	if (!$.support.opacity && opts.cleartype && el.style.filter && style) {
+	if (!$.support.opacity && opts.cleartype && el.style.filter ) {
 		try { el.style.removeAttribute('filter'); }
 		catch(smother) {} // handle old opera versions
 	}
